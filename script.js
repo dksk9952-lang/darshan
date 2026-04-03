34d95ce03a8d024fbc9ce457d6ed7e78d1df415c
Darshan k s <dksk9952@gmail.com>
2026-04-04 00:35:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function cutCake() {
+    document.getElementById("cakeMessage").innerHTML = 
+    "🎉 Happy Birthday My Love ❤️ May your life be filled with happiness, love, and endless smiles 🎂";
+}
